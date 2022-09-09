3183ca509582ec001870b41b1270f2b99e333769
roaddust2 <roaddust2@yahoo.com>
2022-09-09 19:49:53 +0400

    Add flat json compare, add output

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,12 +1,35 @@
 import json
 
 
-def generate_output(list):  # Приведение итера к строке для вывода в консоль
-    return
+def generate_output(list):
+    output = ''
+
+    for item in list:
+        output += ' '.join(item) + '\n'
+    return '{\n' + output + '}'
 
 
-def diff_to_list(diff):  # Выпрямление разницы в итерабельный объект
-    return
+def list_diff(original, changed, diff):
+    output = []
+
+    for key, value in diff.items():
+        if key == 'removed':
+            for j in value:
+                output.append(['-', j + ':', str(original[j])])
+        if key == 'new':
+            for j in value:
+                output.append(['+', j + ':',  str(changed[j])])
+        if key == 'updated':
+            for j in value:
+                output.append(['-', j + ':', str(original[j])])
+                output.append(['+', j + ':', str(changed[j])])
+        if key == 'unchanged':
+            for j in value:
+                output.append([' ', j + ':', str(original[j])])
+
+    output.sort(key=lambda json_key: json_key[1])
+
+    return output
 
 
 def calculate_diff(original, changed):
@@ -46,4 +69,6 @@ def generate_diff(first_file, second_file):
             )
 
     diff = calculate_diff(first_dictionary, second_dictionary)
-    return diff
+    diff_list = list_diff(first_dictionary, second_dictionary, diff)
+
+    return generate_output(diff_list)
