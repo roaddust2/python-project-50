91e2df5a3cd05378c2b528123347feb0f6c67e68
roaddust2 <roaddust2@yahoo.com>
2022-09-09 15:31:05 +0400

    Add calculating diff to dict

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,6 +1,40 @@
 import json
 
 
+def generate_output(list):  # Приведение итера к строке для вывода в консоль
+    return
+
+
+def diff_to_list(diff):  # Выпрямление разницы в итерабельный объект
+    return
+
+
+def calculate_diff(original, changed):
+    x, y = set(original), set(changed)
+
+    new = y.difference(x)
+    removed = x.difference(y)
+    updated, unchanged = set(), set()
+
+    for item in original:
+        memory = []
+        if item in changed and original[item] != changed[item]:
+            memory.append(item)
+            updated.update(memory)
+        if item in changed and original[item] == changed[item]:
+            memory.append(item)
+            unchanged.update(memory)
+
+    output = {
+        'removed': removed,
+        'new': new,
+        'updated': updated,
+        'unchanged': unchanged,
+        }
+
+    return output
+
+
 def generate_diff(first_file, second_file):
     with open(first_file) as original:
         first_dictionary = json.load(
@@ -11,4 +45,5 @@ def generate_diff(first_file, second_file):
                 changed
             )
 
-    return first_dictionary, second_dictionary
+    diff = calculate_diff(first_dictionary, second_dictionary)
+    return diff
