7e275c9cdbda408cf75eaf79b911a853b7239061
roaddust2 <roaddust2@yahoo.com>
2022-10-15 15:20:23 +0400

    New inner view for nested items

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,24 +1,49 @@
+# Inner view example
+#
+# {
+# "name": {
+#   "old_value": old_value
+#   "new_value": new_value,
+#   "state": removed/new/updated/unchanged,
+#   },
+# }
+
 def calculate_diff(original, changed):
-    x, y = set(original), set(changed)
+    diff = {}
+    original_keys = set(original.keys())
+    changed_keys = set(changed.keys())
 
-    new = y.difference(x)
-    removed = x.difference(y)
-    updated, unchanged = set(), set()
+    new_keys = changed_keys.difference(original_keys)
+    for key in new_keys:
+        diff[key] = {
+            "old_value": "",
+            "new_value": changed[key],
+            "state": "new"
+        }
 
-    for item in original:
-        memory = []
-        if item in changed and original[item] != changed[item]:
-            memory.append(item)
-            updated.update(memory)
-        if item in changed and original[item] == changed[item]:
-            memory.append(item)
-            unchanged.update(memory)
+    removed_keys = original_keys.difference(changed_keys)
+    for key in removed_keys:
+        diff[key] = {
+            "old_value": original[key],
+            "new_value": "",
+            "state": "new"
+        }
 
-    output = {
-        'removed': removed,
-        'new': new,
-        'updated': updated,
-        'unchanged': unchanged,
-    }
+    for key in original_keys.intersection(changed_keys):
+        if isinstance(original[key], dict) and isinstance(changed[key], dict):
+            diff[key] = calculate_diff(original[key], changed[key])
 
-    return output
+        elif original[key] == changed[key]:
+            diff[key] = {
+                "old_value": original[key],
+                "new_value": "",
+                "state": "unchanged"
+            }
+        else:
+            diff[key] = {
+                "old_value": original[key],
+                "new_value": changed[key],
+                "state": "unchanged"
+            }
+
+    return diff
