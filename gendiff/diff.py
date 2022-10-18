9409e9d58ab87b30f567737015dc305289dbf3c8
roaddust2 <roaddust2@yahoo.com>
2022-10-18 17:37:27 +0400

    Changed inner view structure

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -2,9 +2,8 @@
 #
 # {
 # "name": {
-#   "old_value": old_value
-#   "new_value": new_value,
-#   "state": removed/new/updated/unchanged,
+#   "state": node/removed/new/updated/unchanged
+#   "value": [value]
 #   },
 # }
 
@@ -16,34 +15,33 @@ def calculate_diff(original, changed):
     new_keys = changed_keys.difference(original_keys)
     for key in new_keys:
         diff[key] = {
-            "old_value": "",
-            "new_value": changed[key],
-            "state": "new"
+            "state": "new",
+            "value": ['', changed[key]],
         }
 
     removed_keys = original_keys.difference(changed_keys)
     for key in removed_keys:
         diff[key] = {
-            "old_value": original[key],
-            "new_value": "",
-            "state": "new"
+            "state": "removed",
+            "value": [original[key], '']
         }
 
     for key in original_keys.intersection(changed_keys):
         if isinstance(original[key], dict) and isinstance(changed[key], dict):
-            diff[key] = calculate_diff(original[key], changed[key])
+            diff[key] = {
+                "state": "node",
+                "value": calculate_diff(original[key], changed[key])
+            }
 
         elif original[key] == changed[key]:
             diff[key] = {
-                "old_value": original[key],
-                "new_value": "",
-                "state": "unchanged"
+                "state": "unchanged",
+                "value": [original[key], '']
             }
         else:
             diff[key] = {
-                "old_value": original[key],
-                "new_value": changed[key],
-                "state": "unchanged"
+                "state": "changed",
+                "value": [original[key], changed[key]]
             }
 
     return diff
