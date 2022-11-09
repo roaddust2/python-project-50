bbdd4ff907afa323e36d6734c4413ef553650732
roaddust2 <roaddust2@yahoo.com>
2022-11-09 16:26:45 +0400

    Sorted + json.dumps + TABS

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,66 +1,93 @@
+from json import dumps
+
+
 MAPPING = {
     "new": "+",
     "removed": "-",
     "unchanged": " ",
     "changed": {
-        "style_old": "-", "value_old": 0,
-        "style_new": "+", "value_new": 1}
+        "style_old": "+", "value_old": 0,
+        "style_new": "-", "value_new": 1}
 }
 
-EQUAL = {
-    True: "false",
-    None: "null"
-}
-
-SPACE = "  "
+SPACE, TAB_NUM = " ", 4
 
 TEMPLATE = "{0}{1} {2}: {3}"
 
 
 def format(diff, depth=1):
 
-    output = []
+    output = ['{']
+    start_tab = calc_tabs(depth).get('start_tab')
+    end_tab = calc_tabs(depth).get('end_tab')
+    new_diff = dict(sorted(diff.items()))
 
-    for key, value in diff.items():
+    for key, value in new_diff.items():
         item_name = key
         item_state = value.get("state")
         item_value = value.get("value")
 
         if item_state == "changed":
             output.append(TEMPLATE.format(
-                SPACE * depth,
+                start_tab,
                 MAPPING[item_state]["style_old"],
                 item_name,
-                item_value[MAPPING[item_state]["value_old"]]))
+                generate_string(
+                    item_value[MAPPING[item_state]["value_old"]],
+                    depth + 1)))
             output.append(TEMPLATE.format(
-                SPACE * depth,
+                start_tab,
                 MAPPING[item_state]["style_new"],
                 item_name,
-                item_value[MAPPING[item_state]["value_new"]]))
+                generate_string(
+                    item_value[MAPPING[item_state]["value_new"]],
+                    depth + 1)))
 
         elif item_state != "node":
             output.append(TEMPLATE.format(
-                SPACE * depth,
+                start_tab,
                 MAPPING[item_state],
                 item_name,
-                item_value[0]))
+                generate_string(item_value[0], depth + 1)))
 
         else:
             output.append(TEMPLATE.format(
-                SPACE * depth,
+                start_tab,
                 ' ',
                 item_name,
                 format(
                     value["value"], depth + 1)))
 
-    return generate_output(output, depth)
+    output.append(end_tab + '}')
+    return '\n'.join(output)
 
 
-def generate_output(list, depth):
-    depth = depth + 1
-    output = ''
-    list.sort(key=lambda item_key: item_key)
+def calc_tabs(depth):
+    tabs = {
+        'start_tab': SPACE * (TAB_NUM * depth - 2),
+        'end_tab': SPACE * (TAB_NUM * (depth - 1))
+    }
+    return tabs
 
-    for item in list:
-        output += ''.join(item) + '\n'
-    return '{\n' + output + (SPACE * depth) + '}'
+
+def generate_string(node, depth):
+    output = []
+    start_tab = calc_tabs(depth).get('start_tab')
+    end_tab = calc_tabs(depth).get('end_tab')
+
+    if isinstance(node, dict):
+        output.append('{')
+        for key, value in node.items():
+            output.append(TEMPLATE.format(
+                start_tab,
+                ' ',
+                key,
+                generate_string(value, depth + 1)
+            ))
+
+        output.append(end_tab + '}')
+    elif isinstance(node, str):
+        output.append(node)
+    else:
+        output.append(dumps(node))
+    return '\n'.join(output)
